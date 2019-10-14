6202cee1a95203ef9fe626e6527645f93a1152a0
bfreeds <5316367+bfreeds@users.noreply.github.com>
2019-10-14 15:50:11 -0400

    Remove default vue create artifacts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Election Results</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "app",
-  components: {
-    HelloWorld
-  },
   created: function() {
     this.$store.dispatch("fetchData");
   }
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
