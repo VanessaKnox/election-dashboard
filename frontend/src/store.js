1575d12cf702d339c81ecf8125ae38892156bf65
bfreeds <5316367+bfreeds@users.noreply.github.com>
2019-10-14 08:29:50 -0400

    Create vuex store with async fetchData action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,8 +3,34 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+// Vuex store registered in main.js, accessible globally as this.$store
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    results: {} // for election geojson, async loaded
+  },
+  // mutations make changes to state
+  mutations: {
+    FETCH_DATA(state, data) {
+      state.results = data;
+    }
+  },
+  // actions called by components to mutate state
+  actions: {
+    fetchData({ commit }) {
+      const dataUrl =
+        "https://gist.githubusercontent.com/bfreeds/16bb7a411f1c88a4d988391071cfd850/raw/ce6be9621ec0452ec3c484e3553ae9eb5baea51e/elections.json";
+
+      // VueAxios plugin instantiated in main.js
+      Vue.axios
+        .get(dataUrl)
+        .then(response => {
+          console.log(response.data);
+
+          commit("FETCH_DATA", response.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
 });
