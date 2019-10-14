f07f7183a110e86f9a90d32f40cebd3df2954f9e
bfreeds <5316367+bfreeds@users.noreply.github.com>
2019-10-14 16:49:29 -0400

    Add SubmitButton with props to receive input & update store.state

---
+++ frontend/src/components/SubmitButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="handleSubmit(data)">Add Result</button>
+</template>
+
+<script>
+export default {
+  props: {
+    result: {
+      type: Object
+    }
+  },
+  methods: {
+    handleSubmit(input) {
+      this.$store.dispatch("addResult", input);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
