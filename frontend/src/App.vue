b7636a8842956ee710304eee5957ae4215b3b52c
bfreeds <5316367+bfreeds@users.noreply.github.com>
2019-10-14 16:51:49 -0400

    Add logic to append <Pre></Pre> to dom

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,32 @@
 <template>
   <div id="app">
     <h1>Election Results</h1>
+    <SubmitButton></SubmitButton>
+
+    <!-- conditionally render element based on newResults array in global state -->
+    <pre class="debug" v-for="(result, index) in newResults" :key="index">
+      {{ result }}
+    </pre>
   </div>
 </template>
 
 <script>
+import SubmitButton from "./components/SubmitButton.vue";
+
 export default {
   name: "app",
+  components: {
+    SubmitButton
+  },
+  // lifecycle method to fetch data
   created: function() {
     this.$store.dispatch("fetchData");
+  },
+  // use computed method to ensure this reloads on store.state changes
+  computed: {
+    newResults() {
+      return this.$store.state.newResults;
+    }
   }
 };
 </script>
