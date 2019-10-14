76b3a84c42c65f9199a47d967fceedd4a6b65215
bfreeds <5316367+bfreeds@users.noreply.github.com>
2019-10-14 17:32:52 -0400

    Create form element to handle new data

---
+++ frontend/src/components/DataForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <form @submit.prevent="handleSubmit()">
+    <div>
+      <label for="state">State:</label>
+      <input v-model="state" type="text" id="state" name="state" />
+    </div>
+    <div>
+      <label for="candidate">Candidate:</label>
+      <input v-model="candidate" type="text" id="candidate" name="candidate" />
+    </div>
+    <div>
+      <label for="votes">Votes:</label>
+      <input v-model="votes" type="number" id="votes" name="votes" />
+    </div>
+    <Button>Submit</Button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    // start with empty object
+    return {
+      state: "",
+      candidate: "",
+      votes: ""
+    };
+  },
+  methods: {
+    handleSubmit() {
+      const result = {
+        state: this.state,
+        candidate: this.candidate,
+        votes: this.votes,
+        timestamp: Date.now()
+      };
+
+      console.log("submitted result data: ", result);
+      // reset form values
+      (this.state = null), (this.candidate = null), (this.votes = null);
+
+      this.$store.dispatch("addResult", result);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
