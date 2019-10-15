536135ccafd3fc9c324aa3f716e0dff95cdafdd8
bfreeds <5316367+bfreeds@users.noreply.github.com>
2019-10-15 07:35:42 -0400

    Create Map component & add to App

---
+++ frontend/src/components/Map.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="map">
+    <h3>Election Map</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    states() {
+      return this.$store.state.states;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#map {
+  border: 1px solid red;
+  height: 300px;
+  width: 500px;
+}
+</style>
