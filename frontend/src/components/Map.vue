f3b13e1f11275d4681a32a7e75050f80cfdbc3b0
bfreeds <5316367+bfreeds@users.noreply.github.com>
2019-10-15 10:31:15 -0400

    Render static map; fix async mapData issue
    This commit fixes the problem that I was encountering
    where the computed mapData was trying to access state.features
    before the state geojson has loaded into Vuex store.
    
    Added a check to only run the d3 math calculations for state paths
    if state.features exists (testing to ensure data is loaded).
    
    Vue's computed properties are reactive, so mapData() will be
    rerun once the computed state() data is available (so state.features)
    is not undefined.
    
    This was also the issue the component was facing with dynamically
    defined width and height, so I switched back from the hard-coded
    temporary fix.
    
    See here https://stackoverflow.com/questions/52121504/computed-property-needs-to-wait-for-async-data

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="map">
-    <h3>Election Map</h3>
-
     <!-- define our svg template -->
     <svg :height="height" :width="width">
       <!-- background -->
@@ -24,32 +22,46 @@
 <script>
 import * as d3 from "d3";
 
-const height = 400;
-const width = 600;
-
 export default {
   data() {
     return {
-      height,
-      width
+      height: null,
+      width: null
     };
   },
   created: function() {
     console.log("Map.vue created");
+  },
+  mounted: function() {
+    // calculate the DOM element's dimensions to bind to svg template
+    console.log(
+      "Map.vue mounted, calculating height and width to pass to D3 visualization."
+    );
+    const rect = this.$el.getBoundingClientRect();
+    console.log({ rect: rect, width: rect.width, height: rect.height });
+    // set component height and width to calculated DOM dimensions
+    this.width = rect.width;
+    this.height = rect.height;
   },
   computed: {
     states() {
       // geojson from vuex store
       return this.$store.state.states;
     },
-    // use d3 math to calculate svg path for template
     mapData() {
+      // make sure states geojson async fetch is completed before trying to calculate path data.  Vue computed properties are reactive - so this will not run while waiting for states to be fetched, but once it's added to Vuex store mapData will update & render the SVG template
+      if (this.states.features === undefined) {
+        return [];
+      }
+      // use d3 math to calculate svg path for template
       const projection = d3
-        .geoConicConformal()
+        .geoAlbersUsa()
         .fitSize([this.width, this.height], this.states);
 
+      // geopath generator function to transform geojson into SVG path
       const path = d3.geoPath(projection);
 
+      // map geojson into bound data to SVG
       return this.states.features.map((feature, index) => {
         return {
           path: path(feature),
@@ -89,7 +101,7 @@ export default {
 }
 
 .state {
-  fill: black;
+  fill: #525252;
   stroke: white;
   stroke-width: 0.5px;
 }
